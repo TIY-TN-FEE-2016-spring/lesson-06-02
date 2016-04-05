21fbf0ef36279f48e1237820955d708cff522377
Ryan Tablada <ryan.tablada@gmail.com>
2016-04-05 10:32:28 -0500

    Looping data

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,10 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return {
-      first: `Homer`,
-      last: `Simpson`,
-      isSmart: false,
-    };
+    return [
+      {
+        first: `Homer`,
+        last: `Simpson`,
+        isSmart: false,
+      },
+      {
+        first: `Marge`,
+        last: `Simpson`,
+        isSmart: true,
+      }
+    ];
   },
 });
